d31c901dfb65bec7f84b53a65b2f3a2df9dc5e80
srushtiiiiiiii <92454053+srushtiiiiiiii@users.noreply.github.com>
2022-03-31 22:04:59 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+### Working Guide
+
+#### To Run the code:
+- Simply run `python main.py`
+- Click 0 to add money to your account first. Money has to be added each time you run the program using step 1.
+- Click 1 to add your expenses
+    - Date: Enter the date of the month in number like 1,2,3,4 etc.
+    - Label: Enter the label as you like to label your expenditure like `all`, `grocery`, `fruits`, etc.
+    - Expense: Money you have spent.
+- Click 2 to take insights of your data at any time
+- Click 3 to take a graphical view of your data.
+- Click 4 to take graphical view of your data between some start and stop date. Date format again in numbers like 1,2,3,etc.
---
+++ budgetTracker.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 26 20:57:08 2020
+Updated on Thu Dec 09 10:17:30 2021
+
+@author: Ayush Choudhary
+"""
+
+
+import os
+import csv
+import datetime
+import matplotlib.pyplot as plt
+
+def add_money():
+   deposit =int(input("Enter the amount to deposit: "))
+   global income
+   income+=deposit
+   
+  
+    
+def enter_data():
+  if not os.path.exists('data.csv'):
+    with open('data.csv','w', newline = "") as file:
+      writer=csv.writer(file)
+      writer.writerow(['Date','Label','Expense'])
+  
+  if os.path.exists('data.csv'):
+    read_file_list = list(csv.reader(open('data.csv','r')))
+    if len(read_file_list) == 0:
+      with open('data.csv','a', newline = "") as file:
+        writer=csv.writer(file)
+        writer.writerow(['Date','Label','Expense'])
+
+  date = int(input("Enter the date: "))
+  label =input("Label your Expense: ")
+  expense= int(input("Expenditure:"))
+
+  with open("data.csv",'a', newline="") as file:
+    writer=csv.writer(file)
+    writer.writerow([date,label,expense])
+
+
+
+def choices():
+  print("0 : Add money to Account.")  
+  print("1 : Enter the data.")
+  print("2 : Insights of data.")
+  print("3 : Graphical view of data.")
+  print("4 : Range graphical view.")
+  
+  try:
+    choice = int(input())
+  except:
+      print("!Enter a valid Value..")
+      
+  return choice
+
+
+
+def data_insights():
+  try:
+    with open('data.csv','r') as file:
+      read = csv.reader(file)
+      read_list=list(read)
+      print(read_list)
+      total_expense=0
+
+      if(income <= 0):
+        print("!! You don't have any income yet.Add some money first.");
+        return
+
+      for each in read_list[1:]:
+        total_expense+=int(each[2])
+        
+      balance=income-total_expense
+      print("Account Balance:",balance)
+      print("Total Expenditure this month:",total_expense)
+      percent=(total_expense/income * 100)
+      print("Expediture is {:.4f}%  of income in {} days of this month.\n".format(percent,len(read_list)-1))
+  except Exception as e:
+     print("It looks like you don't have enough data yet.",e)   
+     
+
+
+def data_visualization():
+  try:
+    with open('data.csv','r') as file:
+      dates=[]
+      expense=[]
+      read=csv.reader(file)
+      file_list=list(read)
+      for each in file_list[1:]:
+        dates.append(int(each[0]))
+        expense.append(int(each[2]))
+   
+      today=datetime.date.today()
+      
+      plt.plot(dates,expense,label='per month')
+      plt.title("--Monthly Expenditure--")
+      plt.xlabel(f"Date {today.month} {today.year}")
+      plt.ylabel("Expense")
+      plt.xticks(dates)  
+      plt.legend()
+      plt.show()
+  except Exception as e:
+    print("It looks like you don't have enough data yet.",e)
+
+
+
+def limited_visualization(start_date,stop_date):
+  try:
+    with open('data.csv','r') as file:
+      dates=[]
+      expense=[]
+      read=csv.reader(file)
+      file_list=list(read)
+      #print(len(file_list))
+      if stop_date > (len(file_list)-1):
+          print("!! Stop date out of range..")
+          return
+      
+      for each in file_list[start_date:stop_date+1]:
+        dates.append(int(each[0]))
+        expense.append(int(each[2]))
+      total_expense=sum(list(map(int,expense)))
+      percent=total_expense/income*100
+      print(f"Total expenditure in this period is {total_expense} which is {percent:.4f}% of the monthly income.")
+      today=datetime.date.today()
+      plt.plot(dates,expense,label='per day range')
+      plt.title("--Range of days Expenditure--")
+      plt.xlabel(f"Date {today.month} {today.year}")
+      plt.ylabel("Expense")
+      plt.xticks(dates)  
+      plt.legend()
+      plt.show()
+  except Exception:
+    print("It looks like you don't have enough data.")
+
+
+
+if __name__=="__main__":
+  income=0
+  
+  print("Select the choice::")
+  running=True
+  while running:
+    choice = choices()
+    if choice ==0: 
+      add_money()
+    elif choice == 1:
+      enter_data()
+    elif choice == 2:
+      data_insights()
+    elif choice == 3:
+      data_visualization()
+    elif choice ==4:
+      print("Specify:")
+      start=int(input("Start Date: "))
+      stop =int(input("Stop Date: "))
+      limited_visualization(start,stop)  
+    else:
+      print("!!INVALID choice.") 
+
+    ask=input("Do you want to continue:(y/n) ")
+    if  ask == 'n' or ask =='N':
+      running = False
+      
